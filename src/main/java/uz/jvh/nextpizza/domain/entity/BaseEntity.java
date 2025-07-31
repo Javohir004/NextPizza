517381e3aab77e3d302689b7fecd5b0e0ee1d86e
Javohir004 <javohirsharifjonov795@gmail.com>
2025-07-31 12:47:30 +0500

    endi boshladim

---
+++ src/main/java/uz/jvh/nextpizza/domain/entity/BaseEntity.java
@@ -0,0 +1,31 @@
+package uz.jvh.nextpizza.domain.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    protected UUID id;
+
+    @CreationTimestamp
+    protected LocalDateTime created;
+
+    @UpdateTimestamp
+    protected LocalDateTime modified;
+
+    protected boolean isActive = true;
+
+}
+
---
+++ src/main/java/uz/jvh/nextpizza/domain/entity/User.java
@@ -0,0 +1,50 @@
+package uz.jvh.nextpizza.domain.entity;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+import uz.jvh.nextpizza.domain.enomerator.UserRole;
+
+
+import java.time.LocalDate;
+
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class User extends BaseEntity {
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    private String surname;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private LocalDate birthDate;
+
+    @Column(nullable = false)
+    @Pattern(regexp = "^\\+998\\s?\\d{9}$", message = "Phone number must start with +998, followed by 9 digits, and may have an optional space.")
+    private String phoneNumber;
+
+
+    @Builder.Default
+    private Double balance = 0.0;
+
+    private String address;
+
+    @Column(nullable = false)
+    private String passportSeries;
+
+}
