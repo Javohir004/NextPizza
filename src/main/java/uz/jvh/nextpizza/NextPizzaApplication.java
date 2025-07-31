fe64471de7382b85226316c65bdf1f141ff040cf
Javohir004 <javohirsharifjonov795@gmail.com>
2025-07-31 12:05:49 +0500

    Initial commit

---
+++ src/main/java/uz/jvh/nextpizza/NextPizzaApplication.java
@@ -0,0 +1,13 @@
+package uz.jvh.nextpizza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NextPizzaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NextPizzaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/jvh/nextpizza/NextPizzaApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.jvh.nextpizza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextPizzaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
