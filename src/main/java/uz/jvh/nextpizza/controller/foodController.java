a3187010e9e85068ca7a4bc580bb8ccf3e016c5f
Javohir004 <javohirsharifjonov795@gmail.com>
2025-08-07 11:11:39 +0500

    ho'sh deyarli yozib bo'ldim endi duo qil xato chiqmay ishlasin

--- src/main/java/uz/jvh/nextpizza/controller/foodController.java
+++ src/main/java/uz/jvh/nextpizza/controller/foodController.java
@@ -2,15 +2,17 @@ package uz.jvh.nextpizza.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import uz.jvh.nextpizza.domain.dto.request.FoodRequest;
+import uz.jvh.nextpizza.domain.dto.request.UserRequest;
 import uz.jvh.nextpizza.domain.dto.response.FoodResponse;
+import uz.jvh.nextpizza.domain.dto.response.UserResponse;
 import uz.jvh.nextpizza.domain.enomerator.FoodType;
+import uz.jvh.nextpizza.domain.entity.User;
 import uz.jvh.nextpizza.service.FoodService;
 
 import java.util.List;
+import java.util.UUID;
 
 @RequestMapping("/api/food")
 @RestController
@@ -29,6 +31,24 @@ public class foodController {
         return ResponseEntity.ok(foodService.searchFoods(name, minPrice, maxPrice, foodType));
     }
 
+
+    @PutMapping("/update/{id}")
+    public FoodResponse updateUser(@PathVariable("id") UUID id, @RequestBody FoodRequest foodRequest) {
+        return foodService.updateFood(id, foodRequest);
+    }
+
+
+    @PostMapping("/create-food")
+    public ResponseEntity<FoodResponse> createUser(@RequestBody FoodRequest foodRequest) {
+        return ResponseEntity.ok(foodService.createFood(foodRequest));
+    }
+
+
+    @DeleteMapping("/delete/{foodId}")
+    public ResponseEntity<String> deleteUser(@PathVariable UUID foodId) {
+        foodService.deleteFoodById(foodId);
+        return ResponseEntity.ok("Foydalanuvchi muvaffaqiyatli o'chirildi.");
+    }
 
 
 }
