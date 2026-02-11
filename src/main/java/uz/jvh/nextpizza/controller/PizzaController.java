317be01e49221607f688137b705ea9e803ffcaf7
Javohir004 <javohirsharifjonov795@gmail.com>
2026-02-11 16:47:41 +0500

    front ga rasmlarni jo'natish va rasmlarni saqlashni yozdim

--- src/main/java/uz/jvh/nextpizza/controller/PizzaController.java
+++ src/main/java/uz/jvh/nextpizza/controller/PizzaController.java
@@ -1,13 +1,17 @@
 package uz.jvh.nextpizza.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import uz.jvh.nextpizza.dto.request.PizzaRequest;
 import uz.jvh.nextpizza.dto.response.PizzaResponse;
 import uz.jvh.nextpizza.enomerator.PizzaType;
+import uz.jvh.nextpizza.service.FileStorageService;
 import uz.jvh.nextpizza.service.PizzaService;
 
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Map;
@@ -18,6 +22,8 @@ import java.util.Map;
 public class PizzaController {
 
     private final PizzaService pizzaService;
+    private final FileStorageService  fileStorageService;
+
 
     @GetMapping("/search")
     public ResponseEntity<List<PizzaResponse>> searchFoods(@RequestParam(required = false) String name,
@@ -50,16 +56,27 @@ public class PizzaController {
         return pizzaService.updateFood(id, pizzaRequest);
     }
 
-    @PostMapping("/create-pizza")
-    public ResponseEntity<PizzaResponse> createUser(@RequestBody PizzaRequest pizzaRequest) {
-        return ResponseEntity.ok(pizzaService.createFood(pizzaRequest));
-    }
+//    @PostMapping("/create-pizza")
+//    public ResponseEntity<PizzaResponse> createUser(@RequestBody PizzaRequest pizzaRequest) {
+//        return ResponseEntity.ok(pizzaService.createFood(pizzaRequest));
+//    }
 
 
     @DeleteMapping("/delete/{foodId}")
     public ResponseEntity<String> deleteUser(@PathVariable Long foodId) {
         pizzaService.deleteFoodById(foodId);
         return ResponseEntity.ok("Pizza muvaffaqiyatli o'chirildi.");
+    }
+
+    @PostMapping(value = "/create-pizza", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PizzaResponse> createPizza(
+            @ModelAttribute PizzaRequest pizzaRequest,
+            @RequestParam("image") MultipartFile image)  throws IOException {
+
+        String fileName = fileStorageService.saveFile(image);
+        pizzaRequest.setImageUrl(fileName);
+
+        return ResponseEntity.ok(pizzaService.createFood(pizzaRequest));
     }
 
 
